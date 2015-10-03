f97d901012fd792678bae8ae5cc23084ad63a5b0
diegoferbp <diegoferbp@igmail.com>
2015-10-03 04:28:09 +0000

    Se crea proyecto nuevamente

--- MyJavaMaven/src/test/java/co/bareno/maven/MyJavaMaven/AppTest.java
+++ MyJavaMaven/src/test/java/co/bareno/maven/MyJavaMaven/AppTest.java
@@ -1,16 +1,38 @@
 package co.bareno.maven.MyJavaMaven;
 
-import static org.junit.Assert.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-import junit.framework.Assert;
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
 
-public class AppTest {
-
-	@Test
-	public void test() {
-		assertTrue(true);
-	}
-
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }
